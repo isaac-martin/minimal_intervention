3d2898628e86c490fc4c9e6553c6c7c7c402204c
isaac-martin <isaac@weareplayground.co>
2019-01-26 15:32:59 -0500

    stylistic changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,7 +16,7 @@ const Layout = ({ children }) => (
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 600,
+        maxWidth: 800,
         padding: `0px 1.0875rem 1.45rem`,
         paddingTop: 0,
         display: `flex`,

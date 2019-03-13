e65205210e5fdd33cddc1d87f4ff6f4699ff650d
Isaac Martin <isaac@weareplayground.co>
2019-03-13 19:17:03 -0400

    fetching data

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -1,16 +1,71 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
-class Quiz extends Component {
-  constructor() {
-    super()
-    this.state = {
-      quizProducts: [],
+function Quiz() {
+  const [wine, setWine] = useState('Red')
+  const [loading, setLoading] = useState(false)
+  const [results, setResults] = useState([])
+
+  const fetchData = wine => {
+    setLoading(true)
+    fetch('https://minimal-intervention.myshopify.com/api/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Shopify-Storefront-Access-Token': '75f418447dd5becdedfd806092ea0145',
+      },
+      body: JSON.stringify({
+        query: `query{
+  products(first: 100 query:"product_type:'${wine}'") {
+    edges {
+      node {
+        productType
+        id
+        title
+        handle
+        descriptionHtml
+        description
+        images(first: 1) {
+          edges {
+            node {
+              originalSrc
+            }
+          }
+        }
+        variants(first:1) {
+          edges {
+            node {
+              price
+            }
+          }
+        }
+      }
     }
   }
-
-  render() {
-    return <div>Wassup</div>
+}`,
+      }),
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then(responseAsJson => {
+        console.log(responseAsJson.data.products.edges)
+        setResults(responseAsJson.data.products.edges)
+        setLoading(false)
+      })
   }
+
+  return (
+    <>
+      {results.length === 0
+      <div className="quiz">
+          <button onClick={() => setWine('White')}>
+            Change Wine To White{' '}
+          </button>
+          <button onClick={() => fetchData(wine)}>Fetch Products</button>
+        </div>
+      }
+    </>
+  )
 }
 
 export default Quiz
